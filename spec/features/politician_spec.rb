2ec74867bc84a9bb17b9e085e62995a3aa6a6272
Keenan Turner & scottsilveus <pair+keenregard.scottsilveus@devbootcamp.com>
2014-06-25 16:18:46 -0500

    user name is printing next to comment

--- spec/features/politician_spec.rb
+++ spec/features/politician_spec.rb
@@ -13,9 +13,21 @@ feature 'User browsing politician profile' do
   it "should have comments printed out" do
     comment = Comment.create(content: "here is all of my content")
     poli.comments << comment
+    user = User.create(username: "scott")
+    user.comments << comment
     visit politician_path(poli)
 
     expect(page).to have_content(comment.content)
+  end
+
+  it "should print out the Commenter's username" do
+    comment = Comment.create(content: "here is all of my content")
+    poli.comments << comment
+    user = User.create(username: "scott")
+    user.comments << comment
+    visit politician_path(poli)
+
+    expect(page).to have_content(user.username)
   end
 
 end
