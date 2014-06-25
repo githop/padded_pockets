21a22b21e2473c34ccd777bd6d8fbf3daa3e05be
Keenan Turner & scottsilveus <pair+keenregard.scottsilveus@devbootcamp.com>
2014-06-25 16:08:15 -0500

    added test for comment to show on page

--- spec/features/politician_spec.rb
+++ spec/features/politician_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
 feature 'User browsing politician profile' do
-  let(:poli) {Politician.create(name: "Myname")}
+  let(:poli) {Politician.create(name: "Myname", id: 1)}
+
 
   it "sees politician name" do
     visit politician_path(poli)
 
     expect(page).to have_content(poli.name)
+  end
+
+  it "should have comments printed out" do
+    comment = Comment.create(content: "here is all of my content")
+    poli.comments << comment
+    visit politician_path(poli)
+
+    expect(page).to have_content(comment.content)
   end
 
 end
--- spec/models/politician_spec.rb
+++ spec/models/politician_spec.rb
@@ -1,5 +1,2 @@
-require 'spec_helper'
 
-describe Politician do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+
