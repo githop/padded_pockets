5c294e615de936e6b52deba0196fea2084b185bf
Milan Grubnic & Tom Hopkins <pair+MilanGrubnic70.githop@devbootcamp.com>
2014-06-25 14:39:16 -0500

    added user sign on with authentication, welcome controller, view, route and error message.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+
+  include BCrypt
+
   has_many :comments
   has_many :votes
+
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -8,7 +10,7 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create]
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
