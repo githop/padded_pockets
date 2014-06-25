95aed53796b881695d9fbcba84e93639321b3bd1
Milan Grubnic & Tom Hopkins <pair+MilanGrubnic70.githop@devbootcamp.com>
2014-06-25 11:00:15 -0500

    adds routes for session, user, and comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+
+  resources :users
+  resources :comments, only: :create
+
+  resources :sessions, only: [:new, :create]
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
