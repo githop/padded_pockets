fc0be54bb323361a338e33a81e70566f2466a253
Keenan Turner & scottsilveus <pair+keenregard.scottsilveus@devbootcamp.com>
2014-06-25 16:37:43 -0500

    add begining of comment form spec

--- spec/features/politician_spec.rb
+++ spec/features/politician_spec.rb
@@ -1,33 +1,45 @@
 require 'rails_helper'
 
 feature 'User browsing politician profile' do
-  let(:poli) {Politician.create(name: "Myname", id: 1)}
+  before(:each) do
+    @poli = Politician.create(name: "Myname", id: 1)
+    @comment =Comment.create(content: "here is all of my content")
+    @user = User.create(username: "scott")
 
+    @user.comments << @comment
+    @poli.comments << @comment
+  end
 
   it "sees politician name" do
-    visit politician_path(poli)
+    visit politician_path(@poli)
 
-    expect(page).to have_content(poli.name)
+    expect(page).to have_content(@poli.name)
   end
 
   it "should have comments printed out" do
-    comment = Comment.create(content: "here is all of my content")
-    poli.comments << comment
-    user = User.create(username: "scott")
-    user.comments << comment
-    visit politician_path(poli)
+    visit politician_path(@poli)
 
-    expect(page).to have_content(comment.content)
+    expect(page).to have_content(@comment.content)
   end
 
   it "should print out the Commenter's username" do
-    comment = Comment.create(content: "here is all of my content")
-    poli.comments << comment
-    user = User.create(username: "scott")
-    user.comments << comment
-    visit politician_path(poli)
+    visit politician_path(@poli)
 
-    expect(page).to have_content(user.username)
+    expect(page).to have_content(@user.username)
   end
 
+  context "create comment form" do
+
+
+    it "should create a comment" do
+      visit politician_path(@poli)
+
+      fill_in "Comment", with: "here is our comment"
+      click_button "Submit"
+
+      expect(page).to have_content("here is our comment")
+
+    end
+
+  end
 end
